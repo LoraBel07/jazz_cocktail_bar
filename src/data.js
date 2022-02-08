47a17dde65c4cc7f2b628ef856bdc9ba8512aa85
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-02-07 22:43:15 -0500

    commit

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import image1 from "img/bahama-mama.jpg";
+import image2 from "img/bloody-mary.jpg";
+
+export const data = [
+	{
+		id: 1,
+		title: "Bahama-Mama",
+		searchTerm: "rum",
+		image: image1
+	},
+	{
+		id: 2,
+		title: "Bloody-Mary",
+		searchTerm: "vodka",
+		image: image2
+	}
+
+]
