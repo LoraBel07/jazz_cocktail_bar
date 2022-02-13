423ed20ee4136236daecc60d7e523e5f6745ee14
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-02-13 09:54:58 -0500

    commit

--- src/Contact.js
+++ src/Contact.js
@@ -1,44 +1,50 @@
 import React from "react";
+import sax from "./saxophone.png";
+import catInCase from "./cat-case.png";
 
 function ContactUs() {
+    const clearInput = () => {
+        window.onbeforeunload = () => {
+            for(const form of document.getElementsByTagName('form')) {
+            form.reset();
+            }
+        }
+    }
 	return(
 		<div>
+            <div>
+			<img src={ sax } className="sax" alt="Saxophone" width="50px"/>
+            <img src={ catInCase } className="catInCase" alt="cat in case" width="270px"/>
+			</div>
 			<div class="container">
 				<h4 class="contText">Would you like to reserve a table? Send us your contact information!</h4>
 				</div>
 			<div>
-			<form action="https://formspree.io/f/xqkwlpdq" method="POST">
-    {/* <!--For right language--> */}
-    <input type="hidden" name="_language" value="en" />
+            <div className="container">
+                <form action="https://formspree.io/f/xqkwlpdq" method="POST">
+                    <input type="hidden" name="_language" value="en" />
 
-    <div class="sweet">
-        <label>
-            Your email:
-            <input type="email" class="text-input" name="_replyto" placeholder="Your email..." required=""/>
-        </label>
-    </div>
+                    <div class="signal contText">
+                        <label> Your email:
+                        <input type="email" class="text-input" name="_replyto" placeholder="Your email..." required=""/>
+                        </label>
+                    </div>
 
-    <div class="sweet">
-        <label>
-            Your message:
-            <textarea class="text-input" name="message" placeholder="Type your message..." required=""></textarea>
-        </label>
-    </div>
-    <br>
-
-    {/* <!-- your other form fields go here --> */}
-
-    <div  class="sweet">
-        <button class="btn" type="submit">Send</button>
-    </div>
-    <br>
-
-    </form>
-			</div>
-
-
-
+                    <div class="signal contText">
+                        <label> Your message:
+                        <textarea class="text-input" name="message" placeholder="Type your message..." required=""></textarea>
+                        </label>
+                    </div>
+                    
+                    <div  class="signal">
+                        <button class="btn" type="submit"  onClick={() => clearInput()}>Send</button>
+                        
+                    </div>
+                    
+                </form>
+            </div>
 		</div>
+</div>
 	)
 }
 
