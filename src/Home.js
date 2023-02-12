aa2214505aea2b6a25e0a4d7c674b3ceaaf37cfd
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2023-02-12 14:27:27 -0500

    commit

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ function Home() {
 			
 		<div className="catHome">			
 			<h3>All we need is here...</h3>
-			<img src={ cat } className="manCat" alt="Cat in case" width="1000px"/>
+			<img src={ cat } className="manCat" alt="Cat in case" width="990px"/>
 
 		</div>	
 		</div>
